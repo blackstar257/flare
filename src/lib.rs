5fbf84cd94a17cec66fc6f5ffb7c64c319c35813
blackstar257 <4362234+blackstar257@users.noreply.github.com>
2025-06-21 20:05:08 -0700

    format

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,14 @@
 use worker::*;
 
 #[event(fetch)]
-async fn fetch(
-    req: Request,
-    env: Env,
-    _ctx: Context,
-) -> Result<Response> {
+async fn fetch(req: Request, env: Env, _ctx: Context) -> Result<Response> {
     console_error_panic_hook::set_once();
-    
+
     Router::new()
         .get_async("/", handle_get)
+        .get_async("/ip", handle_get)
+        .get_async("/debug", handle_debug)
+        .get_async("/time", handle_time)
         .run(req, env)
         .await
 }
@@ -22,4 +21,25 @@ pub async fn handle_get(req: Request, _ctx: RouteContext<()>) -> worker::Result<
     };
     let message = ip.unwrap_or("unknown".to_owned()).to_string();
     Response::ok(message)
+}
+
+pub async fn handle_debug(req: Request, _ctx: RouteContext<()>) -> worker::Result<Response> {
+    let headers = req.headers();
+    let mut headers_map = std::collections::HashMap::new();
+
+    for (name, value) in headers.entries() {
+        headers_map.insert(name, value);
+    }
+
+    Response::from_json(&headers_map)
+}
+
+pub async fn handle_time(_req: Request, _ctx: RouteContext<()>) -> worker::Result<Response> {
+    let now = js_sys::Date::new_0();
+    let datetime_string = now.to_iso_string();
+    Response::ok(
+        datetime_string
+            .as_string()
+            .unwrap_or("Error getting time".to_string()),
+    )
 }
